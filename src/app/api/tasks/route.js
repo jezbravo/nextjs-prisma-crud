7e2c4b90eb3cc4c031489adefb589012b0e66b40
Ezequiel Bravo <126541104+jezbravo@users.noreply.github.com>
2024-02-13 17:43:25 -0300

    feat: :sparkles: Add api methods

--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server";
 
 export function GET() {
+  return NextResponse.json("Obteniendo tareas");
+}
+
+export function POST() {
   return NextResponse.json("Creando tareas");
 }
