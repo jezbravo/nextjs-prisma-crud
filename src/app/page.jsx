25ed14dc2d0b3e075ce7581d19f9ee1a84269a67
Ezequiel Bravo <126541104+jezbravo@users.noreply.github.com>
2024-02-16 19:41:34 -0300

    feat: :sparkles: Update tasks list

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,31 @@
-function HomePage() {
-  return <div>Tareas</div>;
+import { prisma } from "@/libs/prisma";
+
+async function loadTasks() {
+  // const res = await fetch("http://localhost:3000/api/tasks");
+  // const data = await res.json();
+  return await prisma.task.findMany();
+}
+
+async function HomePage() {
+  const tasks = await loadTasks();
+  console.log(tasks);
+
+  return (
+    <section className="container mx-auto">
+      <div className="mt-10 grid grid-cols-3 gap-3">
+        {tasks.map((task) => (
+          <div
+            key={task.id}
+            className="bg-slate-900 p-3 hover:cursor-pointer hover:bg-slate-800"
+          >
+            <h3 className="mb-2 text-2xl font-bold">{task.title}</h3>
+            <p>{task.description}</p>
+            <p>{new Date(task.createdAt).toLocaleDateString()}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default HomePage;
