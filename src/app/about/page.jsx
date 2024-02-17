829e93cd2bc89469b9dd1d01d1ee0389444c0b0e
Ezequiel Bravo <126541104+jezbravo@users.noreply.github.com>
2024-02-17 17:54:10 -0300

    feat: :sparkles: Update about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,45 @@
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <section className="container mx-auto">
+      <h3 className="my-5 text-3xl font-bold">About</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error commodi
+        rem, incidunt ipsa natus saepe id consequuntur, optio reprehenderit
+        distinctio consequatur. Ut reprehenderit consectetur in corrupti aliquam
+        sed magni voluptate. Laboriosam ipsa tempora modi cumque architecto
+        pariatur. Aliquam corporis, totam enim, maiores tenetur consequuntur
+        aperiam vero molestiae, perspiciatis temporibus possimus error sapiente
+        veritatis dolor! Ratione deserunt ex fugit dolores magnam! Nam mollitia
+        rerum molestias necessitatibus, maxime laborum sapiente deleniti.
+        Quibusdam, asperiores exercitationem. Quisquam, numquam. Eveniet,
+        quisquam. Provident qui ut accusantium rem ducimus, libero aperiam
+        debitis voluptates vero unde est veritatis! Vitae corporis quisquam
+        facere, sapiente veniam, et id molestias pariatur laudantium non
+        veritatis animi dolor nemo beatae voluptatem illum, minus delectus
+        voluptatibus iusto mollitia culpa aliquid cumque deserunt! Deleniti,
+        neque? Perspiciatis animi iusto possimus pariatur illum? Atque cumque
+        quibusdam facere, consequatur, officiis voluptate doloremque a
+        blanditiis alias aliquid dicta voluptatem totam unde fugit earum quaerat
+        mollitia pariatur animi? Quis, incidunt. Error nulla quia praesentium
+        exercitationem maiores. Laborum atque cum optio et minus similique.
+        Numquam velit laudantium neque similique dolorum delectus magnam
+        aperiam, suscipit ut. Debitis voluptate temporibus tempora amet alias.
+        Necessitatibus delectus tenetur modi hic, quos veritatis in nesciunt.
+        Temporibus odio aut accusantium. Id corrupti cumque voluptatibus, iusto,
+        pariatur expedita itaque officiis labore esse amet, quo consequuntur
+        minima voluptates et? Ducimus veritatis cum veniam et, laudantium
+        blanditiis labore explicabo vitae exercitationem odio dolor laborum modi
+        nulla harum est ratione repellat iste fuga omnis, architecto sequi!
+        Eaque cupiditate rem facilis dolorum. In inventore quam eaque mollitia
+        laudantium. Nam nostrum nihil aperiam libero aliquam. Expedita ratione
+        quis at incidunt aliquam ab soluta optio hic tempora, neque consequatur
+        qui et quaerat modi exercitationem. Temporibus molestias porro laborum
+        nisi maiores in. Vitae, alias distinctio esse, illo placeat iure
+        provident omnis delectus animi illum, deleniti eius. Quia quae
+        aspernatur ab tenetur nesciunt? Voluptas, vitae dignissimos!
+      </p>
+    </section>
+  );
 }
 
 export default AboutPage;
