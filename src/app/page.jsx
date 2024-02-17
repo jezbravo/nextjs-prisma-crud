10fac4fcee59441db8f39438a1417b2b05c32226
Ezequiel Bravo <126541104+jezbravo@users.noreply.github.com>
2024-02-17 19:06:43 -0300

    Update files

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,6 +7,8 @@ async function loadTasks() {
   return await prisma.task.findMany();
 }
 
+export const revalidate = 0;
+
 async function HomePage() {
   const tasks = await loadTasks();
   console.log(tasks);
