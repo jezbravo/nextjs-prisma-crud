2a6f997a776eebd6f963b4c373f9a577281f7272
Ezequiel Bravo <126541104+jezbravo@users.noreply.github.com>
2024-02-17 10:52:19 -0300

    feat: :sparkles: Create Update Task logic

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,23 +1,49 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-function NewPage() {
+function NewPage({ params }) {
   const router = useRouter();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    if (params.id) {
+      fetch(`/api/tasks/${params.id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setTitle(data.title);
+          setDescription(data.description);
+        });
+    }
+  }, []);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const title = e.target.title.value;
-    const description = e.target.description.value;
-    const res = await fetch("/api/tasks", {
-      method: "POST",
-      body: JSON.stringify({ title, description }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    const data = await res.json();
-    console.log(data);
+    // const title = e.target.title.value;
+    // const description = e.target.description.value;
+    if (params.id) {
+      const res = await fetch(`/api/tasks/${params.id}`, {
+        method: "PUT",
+        body: JSON.stringify({ title, description }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      console.log(data);
+    } else {
+      const res = await fetch("/api/tasks", {
+        method: "POST",
+        body: JSON.stringify({ title, description }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+    }
     router.push("/");
+    router.refresh();
   };
 
   return (
@@ -34,6 +60,8 @@ function NewPage() {
           type="text"
           className="mb-4 w-full border border-gray-400 p-2 text-black"
           placeholder="Título"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
         />
         <label htmlFor="description" className="text-sm font-bold">
           Descripción
@@ -43,6 +71,8 @@ function NewPage() {
           rows="3"
           className="mb-4 w-full border border-gray-400 p-2 text-black"
           placeholder="Descripción"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
         ></textarea>
         <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
           Crear
